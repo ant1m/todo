48020e045b5bce86f112b9e49b0dc011dc7a4546
Jacques-Antoine Massé <jacquesantoine.masse@orange.com>
2014-12-02 18:29:08 +0100

    Test de handler

--- server/server_test.go
+++ server/server_test.go
@@ -1,10 +1,13 @@
 package server
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"testing"
 	"time"
+
+	"github.com/ant1m/todo/tasks"
 )
 
 func TestGetTasks(t *testing.T) {
@@ -21,5 +24,32 @@ func TestGetTasks(t *testing.T) {
 	if json, _ := ioutil.ReadAll(r.Body); string(json) != "[]\n" {
 		t.Error("Tasks should be an empty json array, [], but was", string(json))
 	}
+}
 
+type fakeRespWriter struct {
+	http.ResponseWriter
+	spy []byte
+}
+
+func (f *fakeRespWriter) Write(b []byte) (int, error) {
+	f.spy = b
+	return len(b), nil
+}
+
+func (f *fakeRespWriter) WriteHeader(h int) {
+	// does nothing, just a stub
+}
+
+func TestGetTasksHandler(t *testing.T) {
+	context := &AppContext{
+		tasks.NewTaskManager(),
+	}
+	fr := &fakeRespWriter{}
+	req, _ := http.NewRequest("GET", "bla", bytes.NewReader([]byte("bla")))
+	handler := tasksHandler(context)
+	handler(fr, req)
+	json := string(fr.spy)
+	if json != "[]\n" {
+		t.Error("Tasks should be an empty json array, [], but was", json)
+	}
 }
